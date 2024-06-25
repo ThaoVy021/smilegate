2514c6296db16f57a30927b1b776952ca04aa8cd
ThaoVy <thaovyluuthanh@gmail.com>
2024-06-25 14:32:59 +0700

    create test

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Main from "../../layouts/Main";
+import Content from "../../layouts/Content";
+
+export default function Home() {
+  return (
+    <>
+      <Main />
+      <Content />
+    </>
+  );
+}
